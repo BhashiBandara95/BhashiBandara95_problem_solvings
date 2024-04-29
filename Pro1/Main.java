c80b653fe5768298f487639e3acc4df973465bda
BhashiBandara95 <ugabkanishka@gmail.com>
2024-04-30 01:16:38 +0530

    Solve Pro1 using Java

---
+++ Pro1/Main.java
@@ -0,0 +1,15 @@
+public class Main{
+    public static void main(String[] args){
+       // declare the initial variable
+        int x = 1;
+        // loop the numbers 1 to 10
+        while(x <= 10){
+            // check the even numbers
+            if(x % 2 == 0){
+                System.out.println(x);
+            }
+            // increment the number
+            x ++;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # BhashiBandara95_problem_solvings
-This repo use to showcase my problem solving skill in different languages.
+
+This repo is used to showcase my problem-solving skills in different languages. This report includes also a flowchart and pseudocode.
+
+- ### Problem 01
+
+**Print even numbers from 1 to 10.**
